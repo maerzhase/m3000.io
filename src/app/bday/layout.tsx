fe5f82cd233e1cecf03d0698228a24814afd48a9
maerzhase <markus@lili16.de>
2025-09-29 21:00:18 +0100

    fix bday image

--- src/app/bday/layout.tsx
+++ src/app/bday/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     siteName: "cat's 40s birthday surprise",
     images: [
       {
-        url: "https://m3000.io/bday-image.png",
+        url: "https://m3000.io/bday-image.jpg",
         width: 1200,
         height: 630,
         alt: "m3000.io — Design engineering",
